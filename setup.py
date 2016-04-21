ea889e83d96ba3ecb1dd43c0ab47ce191d1b3b67
ale88andr <ale88andr@gmail.com>
2016-04-21 20:45:08 +0300

    rebuild project

---
+++ setup.py
@@ -0,0 +1,13 @@
+from distutils.core import setup
+
+setup(
+    name='workflow',
+    version='0.0.0.1',
+    packages=['workflow', 'workflow.settings', 'documents', 'documents.migrations', 'foundation',
+              'foundation.migrations'],
+    url='',
+    license='',
+    author='tol',
+    author_email='ale88andr@gmail.com',
+    description='Workflow service'
+)
